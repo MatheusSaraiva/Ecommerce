cbb596e11d8dc71e09713d2a36406581347c9462
Matheus Saraiva <saraivarego@gmail.com>
2021-10-22 15:59:26 -0300

    Cadastro de usuários (parte 1)

--- loja/settings.py
+++ loja/settings.py
@@ -40,11 +40,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    'crispy_forms',
     # TODO: Remover debug toolbar
     'debug_toolbar',
 ]
-
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
